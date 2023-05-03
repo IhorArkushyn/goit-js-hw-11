ce4a90f1b2760e42f5813a721ac9647240962cd5
Ihor Arkushyn <snapscan68@gmail.com>
2023-05-03 15:39:00 +0300

    return to loadMoreBtn

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 import './css/styles.css';
 import { fetchPictures } from './js/fetchPictures';
 import { createPictureMarkup } from './js/pictureMarkup';
-// import { onScroll } from './js/scroll';
+import { onScroll } from './js/scroll';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const guard = document.querySelector('.js-guard');
+// const guard = document.querySelector('.js-guard');
 const form = document.querySelector('#search-form');
 const pictureContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a');
 const perPage = 40;
-const options = {
-  root: null,
-  rootMargin: '500px',
-  threshold: 0,
-};
-const observer = new IntersectionObserver(onPagination, options);
+// const options = {
+//   root: null,
+//   rootMargin: '500px',
+//   threshold: 0,
+// };
+// const observer = new IntersectionObserver(onPagination, options);
 
 let query = '';
 let totalPages = null;
-let page = 1;
+let page = null;
 
 form.addEventListener('submit', onSearchForm);
 
-// loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
 loadMoreBtn.style.display = 'none';
 
 async function onSearchForm(e) {
   e.preventDefault();
-
+  page = 1;
   query = e.currentTarget.searchQuery.value.trim();
   pictureContainer.innerHTML = '';
-  // loadMoreBtn.style.display = 'none';
+  loadMoreBtn.style.display = 'none';
 
   if (query === '') {
     Notify.failure(
@@ -52,16 +52,16 @@ async function onSearchForm(e) {
       createPictureMarkup(data.hits);
       totalPages = data.totalHits / perPage;
 
-      if (page !== totalPages) {
-        observer.observe(guard);
-      }
+      // if (page !== totalPages) {
+      //   observer.observe(guard);
+      // }
 
       lightbox.refresh();
       Notify.success(`Hooray! We found ${data.totalHits} images.`);
 
-      // if (data.totalHits > perPage) {
-      //   loadMoreBtn.style.display = 'block';
-      // }
+      if (data.totalHits > perPage) {
+        loadMoreBtn.style.display = 'block';
+      }
     }
   } catch (error) {
     console.log(error);
@@ -70,59 +70,83 @@ async function onSearchForm(e) {
   }
 }
 
-async function onPagination(entries, observer) {
-  entries.forEach(async entry => {
-    if (entry.isIntersecting) {
-      page += 1;
-      try {
-        const { data } = await fetchPictures(query, page, perPage);
-        createPictureMarkup(data.hits);
-        lightbox.refresh();
+//  function onPagination(entries, observer) {
+//   entries.forEach(async entry => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       page += 1;
+//       try {
+//         const { data } = await fetchPictures(query, page, perPage);
+//         createPictureMarkup(data.hits);
+//         lightbox.refresh();
 
-        // onScroll();
+//         // onScroll();
 
-        totalPages = data.totalHits / perPage;
+//         totalPages = data.totalHits / perPage;
 
-        if (page > totalPages) {
-          observer.unobserve(guard);
-          // loadMoreBtn.style.display = 'none';
-          Notify.failure(
-            "We're sorry, but you've reached the end of search results."
-          );
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  });
-}
+//         if (page > totalPages) {
+//           observer.unobserve(guard);
+//           // loadMoreBtn.style.display = 'none';
+//           Notify.failure(
+//             "We're sorry, but you've reached the end of search results."
+//           );
+//         }
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//   });
+// }
 
 // function onLoadMoreBtn() {
 //   page += 1;
 
-// function onPagination(entries, observer) {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       page += 1;
-//       fetchPictures(query, page, perPage)
-//         .then(({ data }) => {
-//           createPictureMarkup(data.hits);
-//           lightbox.refresh();
+//   // function onPagination(entries, observer) {
+//   //   entries.forEach(entry => {
+//   //     if (entry.isIntersecting) {
+//   // page += 1;
+//   fetchPictures(query, page, perPage)
+//     .then(({ data }) => {
+//       createPictureMarkup(data.hits);
+//       lightbox.refresh();
 
-//           // onScroll();
+//       onScroll();
 
-//           totalPages = data.totalHits / perPage;
+//       totalPages = data.totalHits / perPage;
 
-//           if (page > totalPages) {
-//             observer.unobserve(guard);
-//             // loadMoreBtn.style.display = 'none';
-//             Notify.failure(
-//               "We're sorry, but you've reached the end of search results."
-//             );
-//           }
-//         })
-//         .catch(error => console.log(error));
-//     }
-//   });
-
+//       if (page > totalPages) {
+//         observer.unobserve(guard);
+//         loadMoreBtn.style.display = 'none';
+//         Notify.failure(
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//       }
+//     })
+//     .catch(error => console.log(error));
 // }
+
+
+async function onLoadMoreBtn() {
+  page += 1;
+
+  try {
+    const { data } = await fetchPictures(query, page, perPage);
+
+    createPictureMarkup(data.hits);
+    lightbox.refresh();
+
+    onScroll();
+
+    const totalPages = data.totalHits / perPage;
+
+    if (page > totalPages) {
+      observer.unobserve(guard);
+      loadMoreBtn.style.display = 'none';
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
